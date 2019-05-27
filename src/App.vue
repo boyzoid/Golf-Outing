4122c79d54dbe78119b4b0372cd6f126021eed9a
Scott Stroz <scott.stroz@gmail.com>
2019-05-27 13:02:44 -0400

    Basic updates to handle custom CSS

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,11 @@
 </template>
 
 <style lang="scss">
+  @import './assets/styles/custom.scss';
+  @import '../node_modules/bootstrap/scss/bootstrap.scss';
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 #nav {
   padding: 30px;
