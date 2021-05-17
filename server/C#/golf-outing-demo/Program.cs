d6ee7d3f64e2c9873c58d398b70f1df4ce0365ac
Scott Stroz <scott.stroz@gmail.com>
2021-05-17 16:14:02 -0400

    Added C#

---
+++ server/C#/golf-outing-demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace golf_outing_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fuck off!!!");
+        }
+    }
+}
